30f0008c5fa57d24960013b991dd28eee5e6d15b
Andy Miller <amcoder@gmail.com>
2016-02-27 20:39:27 -0800

    Add tests or SnakeCasePropertyNameResolver

---
+++ Curse.RestProxy.Tests/Serialization/SnakeCasePropertyNamesContractResolverTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Curse.RestProxy.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Curse.RestProxy.Tests.Serialization
+{
+    [TestClass]
+    public class SnakeCasePropertyNamesContractResolverTests
+    {
+        private IDictionary<string, string> TestCases = new Dictionary<string, string>
+        {
+            { "already_snake", "already_snake" },
+            { "ALLUPPER", "allupper" },
+            { "alllower", "alllower" },
+            { "PascalCase", "pascal_case" },
+            { "camelCase", "camel_case" },
+            { "ACRONYMName", "acronym_name" },
+            { "SomeACRONYM", "some_acronym" }
+        };
+
+        [TestMethod]
+        public void ResolvesCorrectSnakeCaseNames()
+        {
+            var resolver = new SnakeCasePropertyNamesContractResolver();
+
+            foreach(var kvp in TestCases)
+            {
+                var result = resolver.GetResolvedPropertyName(kvp.Key);
+                Assert.AreEqual(kvp.Value, result,
+                    "{0} should resolve to {1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
