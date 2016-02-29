9e1271cb54463cf40eb092dd8e9535c06d91f7ea
Andy Miller <amcoder@gmail.com>
2016-02-28 19:44:32 -0500

    Add the addon controller and `GetAddOn` action

---
+++ Curse.RestProxy.Tests/Controllers/AddOnControllerGetAddOnTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Curse.RestProxy.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Curse.RestProxy.Tests.Controllers
+{
+    [TestClass]
+    public class AddOnControllerGetAddOnTests
+    {
+        [TestMethod]
+        public void GetAddOnReturnsOkWhenAddOnFound()
+        {
+            var addOn = new AddOnService.AddOn();
+            var addOnService = Mock.Of<AddOnService.IAddOnService>(s =>
+                s.GetAddOnAsync(1) == Task.FromResult(addOn)
+            );
+
+            var controller = new AddOnController(addOnService);
+
+            var result = controller.GetAddOn(1).Result;
+
+            Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<AddOnService.AddOn>),
+                "GetAddOn should return Ok when the addon is found");
+        }
+
+        [TestMethod]
+        public void GetAddOnReturnsResultFromAddOnService()
+        {
+            var addOn = new AddOnService.AddOn();
+            var addOnService = Mock.Of<AddOnService.IAddOnService>(s =>
+                s.GetAddOnAsync(1) == Task.FromResult(addOn)
+            );
+
+            var controller = new AddOnController(addOnService);
+
+            var result = controller.GetAddOn(1).Result as OkNegotiatedContentResult<AddOnService.AddOn>;
+
+            Assert.AreEqual(addOn, result.Content,
+                "GetAddOn should return result from the addon service");
+        }
+
+        [TestMethod]
+        public void GetAddOnReturnsNotFoundWhenAddOnDoesNotExist()
+        {
+            var addOnService = Mock.Of<AddOnService.IAddOnService>(s =>
+                s.GetAddOnAsync(1) == Task.FromResult((AddOnService.AddOn)null)
+            );
+
+            var controller = new AddOnController(addOnService);
+
+            var result = controller.GetAddOn(1).Result;
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult),
+                "GetAddOn should return not found when addon does not exist");
+        }
+    }
+}
---
+++ Curse.RestProxy/Controllers/AddOnController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Curse.RestProxy.Controllers
+{
+    [RoutePrefix("api/addon")]
+    public class AddOnController : ApiController
+    {
+        private AddOnService.IAddOnService AddOnService { get; set; }
+
+        public AddOnController(AddOnService.IAddOnService addOnService)
+        {
+            AddOnService = addOnService;
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetAddOn")]
+        public async Task<IHttpActionResult> GetAddOn(int id)
+        {
+            var result = await AddOnService.GetAddOnAsync(id);
+
+            if(result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
